973d8e383f644af848620f7e714dade340e5ed46
ihsan favian <ihsanfavian831@gmail.com>
2025-10-24 14:53:16 +0700

    Add files via upload

--- match.py
+++ match.py
@@ -61,3 +61,28 @@ match hari:
     case 6| 7:
         print("yeay libur")    
 
+#belajar berhitung
+a = int(input("masukan angka pertama: "))
+b = int(input("masukan angka kedua: "))
+operator = input("masukan operator (+, -, *, /): ")
+
+match operator:
+    case "+":
+        hasil = a + b
+        print(f"Hasil dari {a} + {b} = {hasil}")
+    case "-":
+        hasil = a - b 
+        print(f"Hasil dari {a} - {b} = {hasil}")
+    case "*":
+        hasil = a * b
+        print(f"Hasil dari {a} * {b} = {hasil}")
+    case "/":
+        if b != 0:
+            hasil = a / b
+            print(f"Hasil dari {a} / {b} = {hasil}")
+        else:
+            print("Error: Pembagian dengan nol tidak bisa")    
+    case _:
+        print("operator tidak di kenali!")        
+            
+        
